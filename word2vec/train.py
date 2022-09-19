f8081570da69d1f0e3c6e1db9016e38a0c02b9b4
solace6282 <gkataro4@gmail.com>
2022-09-20 01:04:09 +0700

    classification done

--- word2vec/train.py
+++ word2vec/train.py
@@ -5,32 +5,72 @@ from data import load_with_datasets
 # from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 # create stemmer
 import gensim
-from gensim import corpora, similarities
-from gensim.models import Word2Vec, KeyedVectors
+import numpy as np
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score, recall_score
 
 
 def start_train():
   data = load_with_datasets()
+
   text = data['train']['text']
   labels = data['train']['label']
 
-  
-  
-  # updated = train.map(utils_preprocess_text)
+  test_text = data['test']['text']
+  test_labels = data['test']['label']
+
   tokenized = []
   for i in range(0, len(text)):
-    # text[i] = utils_preprocess_text(text[i])
     tokenized.append(utils_preprocess_text(text[i]))
-  all_words = sum(tokenized, [])
+  # all_words = sum(tokenized, [])
+
+  test_tokenized = []
+  for i in range(0, len(test_text)):
+    test_tokenized.append(utils_preprocess_text(test_text[i]))
 
   corpus = tokenized + labels
-  model = gensim.models.Word2Vec(tokenized, min_count=1)
+  
+  labels = list(map(lambda x: 1 if x == 'yes' else 0, labels))
+  test_labels = list(map(lambda x: 1 if x == 'yes' else 0, test_labels))
+
+  X_train = tokenized
+  y_train = labels
+  X_test = test_tokenized
+  y_test = test_labels
+
+  # bisa ad parameter vector_size, window
+  model = gensim.models.Word2Vec(X_train, min_count=1)
   model.wv.most_similar('coronavirus')
 
-  # count = nltk.FreqDist(all_words)
+  words = set(model.wv.index_to_key)
+  X_train_vect = np.array([np.array([model.wv[i] for i in ls if i in words]) for ls in X_train])
+  X_test_vect = np.array([np.array([model.wv[i] for i in ls if i in words]) for ls in X_test])
 
-    
-  print(model)
+  X_train_vect_avg = []
+  for v in X_train_vect:
+    if v.size:
+        X_train_vect_avg.append(v.mean(axis=0))
+    else:
+        X_train_vect_avg.append(np.zeros(100, dtype=float))
+          
+  X_test_vect_avg = []
+  for v in X_test_vect:
+    if v.size:
+        X_test_vect_avg.append(v.mean(axis=0))
+    else:
+        X_test_vect_avg.append(np.zeros(100, dtype=float))
+  
+  rf = RandomForestClassifier()
+  rf_model = rf.fit(X_train_vect_avg, y_train)
+
+  y_pred = rf_model.predict(X_test_vect_avg)
+
+  precision = precision_score(y_test, y_pred)
+  recall = recall_score(y_test, y_pred)
+  print('Precision: {} / Recall: {} / Accuracy: {}'.format(
+      round(precision, 3), round(recall, 3), round((y_pred==y_test).sum()/len(y_pred), 3)))
+  
+  # print(X_test_vect)
   pass
 
 def utils_preprocess_text(text):
