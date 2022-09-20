b1f3ba46de354ba0bfea85f0f67338f6d11b12ef
Rizky I. F <rykzius@gmail.com>
2022-09-20 19:28:07 +0700

    feat: add evaluation per epoch

--- bert/train.py
+++ bert/train.py
@@ -1,39 +1,59 @@
+import numpy as np
+from data import load_with_datasets as load_data
 from transformers import AutoTokenizer, DataCollatorWithPadding, AutoModelForSequenceClassification, TrainingArguments, Trainer
-from data import load_with_datasets
+from evaluate import load as load_metric
 
+# define training hyperparameters
 TRAINING_ARGUMENTS = [
   TrainingArguments(
-    output_dir="./bert/results/5-8",
-    learning_rate=5e-5,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    num_train_epochs=1
-  ),
-  TrainingArguments(
-    output_dir="./bert/results/2-16",
-    learning_rate=2e-5,
+    output_dir="./bert/results/4-16",
+    learning_rate=1e-5,
     per_device_train_batch_size=16,
     per_device_eval_batch_size=16,
-    num_train_epochs=1
+    num_train_epochs=25,
+    evaluation_strategy='epoch'
+  ),
+  TrainingArguments(
+    output_dir="./bert/results/1-16",
+    learning_rate=4e-5,
+    per_device_train_batch_size=16,
+    per_device_eval_batch_size=16,
+    num_train_epochs=25,
+    evaluation_strategy='epoch'
   ),
   TrainingArguments(
     output_dir="./bert/results/2-8",
-    learning_rate=2e-5,
-    per_device_train_batch_size=8,
-    per_device_eval_batch_size=8,
-    num_train_epochs=1
+    learning_rate=4e-5,
+    per_device_train_batch_size=32,
+    per_device_eval_batch_size=32,
+    num_train_epochs=25,
+    evaluation_strategy='epoch'
   )
 ]
 
+# training function
 def start_train():
+  # load and tokenize data
   tokenizer = AutoTokenizer.from_pretrained("indolem/indobert-base-uncased")
-  tokenized_data = load_with_datasets().map(lambda x: {
+  tokenized_data = load_data().map(lambda x: {
     "text": x["text"],
     "label": 1 if x["label"] == 'yes' else 0
   }).map(lambda x: tokenizer(x['text']))
+
+  # create data collator
   data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+  # load pretrained model
   model = AutoModelForSequenceClassification.from_pretrained("indolem/indobert-base-uncased", num_labels=2)
 
+  # load accuracy metric function
+  metric = load_metric("accuracy")
+  def compute_metrics(eval_pred):
+      logits, labels = eval_pred
+      predictions = np.argmax(logits, axis=-1)
+      return metric.compute(predictions=predictions, references=labels)
+
+  # start training for each hyperparameter
   for training_args in TRAINING_ARGUMENTS:
     trainer = Trainer(
         model=model,
@@ -42,5 +62,6 @@ def start_train():
         eval_dataset=tokenized_data["test"],
         tokenizer=tokenizer,
         data_collator=data_collator,
+        compute_metrics=compute_metrics
     )
     trainer.train()
